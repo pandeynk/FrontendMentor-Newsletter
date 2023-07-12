21a2e820c0eb5b8b1712dc1ceeb6feba4d5305de
Nandan Pandey <mepandeyn@gmail.com>
2023-07-12 22:05:26 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const handleSubscribe = function () {
+  const emailEl = document.getElementById("email");
+  const errorTextEl = document.getElementById("errorText");
+  const newsletterDialogEl = document.getElementById("newsletterDialog");
+  const successDialogEl = document.getElementById("successDialog");
+  const emailTextEl = document.getElementById("emailText");
+
+  if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailEl.value)) {
+    emailEl.className = "";
+    errorTextEl.style.display = "none";
+    newsletterDialogEl.style.display = "none";
+    successDialogEl.style.display = "block";
+    emailTextEl.innerText = emailEl.value;
+    emailEl.value = "";
+  } else {
+    emailEl.className = "error";
+    errorTextEl.style.display = "block";
+  }
+};
+
+const handleDismiss = function () {
+  const emailEl = document.getElementById("email");
+  const errorTextEl = document.getElementById("errorText");
+  const newsletterDialogEl = document.getElementById("newsletterDialog");
+  const successDialogEl = document.getElementById("successDialog");
+
+  emailEl.className = "";
+  errorTextEl.style.display = "none";
+  newsletterDialogEl.style.display = "block";
+  successDialogEl.style.display = "none";
+};
